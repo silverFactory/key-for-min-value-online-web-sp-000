aeb004637e9ba6f7911c3d32c0b56ad1ccce5bd5
Will Studabaker <will.studabaker@gmail.com>
2020-10-28 16:40:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == nil
+    return nil
+  else
+    min_key = 0
+    name_hash.each do |name, value|
+    #if the current key's value is less than the previous key's value,
+    #assign current key to min_key
+    
+    end
+    min_key
+  end
 end
