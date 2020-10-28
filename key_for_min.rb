1449c8c00a1278b2e741b1d843df3ae880c8d7e4
Will Studabaker <will.studabaker@gmail.com>
2020-10-28 16:56:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,7 +10,7 @@ def key_for_min_value(name_hash)
     name_hash.each do |name, value|
     #if the current key's value is less than the previous key's value,
     #assign current key to min_key
-      if i = 0
+      if i == 0
         min_key = name
         i +=1
       elsif name_hash[min_key] > value
